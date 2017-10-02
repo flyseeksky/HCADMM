142816d04e5e60bb9318b48bc77fcef62b34c2c7
flyseeksky <mamengchn@gmail.com>
2017-10-02 18:51:16 -0500

    generate hyper incidence matrix from simple graph incidence matrix

--- GraphGenerator.py
+++ GraphGenerator.py
@@ -4,11 +4,11 @@ import numpy as np
 import scipy as sp
 
 
-def check_hyper_edges(simple_adj, hyper_edges):
-    assert isinstance(simple_adj, np.ndarray)
+def check_hyper_edges(incidence, hyper_edges):
+    assert isinstance(incidence, np.ndarray)
     assert isinstance(hyper_edges, list)
     # verify total number of nodes in hyperedges are consistent
-    n_edges = simple_adj.shape[1]
+    n_edges = incidence.shape[1]
     total_edges = 0
     for edge in hyper_edges:
         if isinstance(edge, list):
@@ -18,15 +18,15 @@ def check_hyper_edges(simple_adj, hyper_edges):
     assert total_edges == n_edges
 
 
-def node_to_edge_list(simple_adj, node_list):
+def node_to_edge(incidence, node_list):
     """
     Convert hyper identifiers from node list to edge list
-    :param simple_adj: adjacency matrix for simple graph
+    :param incidence: adjacency matrix for simple graph
     :param node_list: nodes list of each hyper edge
     :return:
     """
     edge_list = []
-    adj = np.array(simple_adj)
+    adj = np.array(incidence)
     all_edges = np.arange(adj.shape[1])
     for nodes in node_list:
         sub_adj = adj[nodes, :]
@@ -37,7 +37,7 @@ def node_to_edge_list(simple_adj, node_list):
 
 def random_connected_graph(n_nodes, prob):
     """
-    randomly generate a graph using Erdos-Renyi model
+    randomly generate a connected graph using Erdos-Renyi model
     :param n_nodes: number of nodes
     :param prob: the probability of an edge
     :return: an Networkx object
@@ -50,21 +50,21 @@ def random_connected_graph(n_nodes, prob):
     return G
 
 
-def simple_adj_to_hyper_adj(simple_adj, hyper_edges):
+def hyper_incidence(incidence, hyper_edges):
     """
-    This function convert an adjacency matrix of a simple graph to an adjacency of hypergraph, whose
+    This function convert an incidence matrix of a simple graph to an incidence of hypergraph, whose
     edges specified by parameter hyper_edges
-    :param simple_adj: a matrix
+    :param incidence: a matrix
     :param hyper_edges: a list of hyper edges, each of which is a list of nodes
     :return:
     """
-    check_hyper_edges(simple_adj, hyper_edges)
-    hyper_adj = np.zeros((simple_adj.shape[0], len(hyper_edges)), dtype=np.int)
-    for n, edge in enumerate(hyper_edges):
-        if isinstance(edge, list) and len(edge) > 1: # if edge is a list, sub_adj is a ndarray
-            sub_adj = simple_adj[:, edge]
-            sub_hyper_adj = sub_adj.sum(axis=1) > 0
-        else:                                        # if edge is a single number, sub_adj is a vector
-            sub_hyper_adj = simple_adj[:, edge]
-        hyper_adj[:, n] = sub_hyper_adj.astype(np.int).ravel()
-    return hyper_adj
+    check_hyper_edges(incidence, hyper_edges)
+    hyper_incidence = np.zeros((incidence.shape[0], len(hyper_edges)), dtype=np.int)
+    for n, edge in enumerate(hyper_edges):  # assuming edges are ascending ordered
+        if isinstance(edge, list) and len(edge) > 1: # if edge is a list, sub_incidence is a ndarray
+            sub_incidence = incidence[:, edge]
+            sub_hyper_incidence = sub_incidence.sum(axis=1) > 0
+        else:                                        # if edge is a single number, sub_incidence is a vector
+            sub_hyper_incidence = incidence[:, edge]
+        hyper_incidence[:, n] = sub_hyper_incidence.astype(np.int).ravel()
+    return hyper_incidence
--- test_functions.py
+++ test_functions.py
@@ -13,11 +13,11 @@ def test_hyper_adj():
                   [0, 1, 0, 0],
                   [0, 0, 1, 1],
                   [0, 0, 0, 1]])
-    B = simple_adj_to_hyper_adj(A, [[0,1,2], [3]])
+    B = hyper_incidence(A, [[0, 1, 2], [3]])
     C = np.array([[1, 0], [1, 0], [1, 0], [1, 1], [0, 1]])
     C2 = np.array([[1], [1], [1], [1], [1]])
-    B1 = simple_adj_to_hyper_adj(A, [0, 1, 2, 3])
-    B2 = simple_adj_to_hyper_adj(A, [[0, 1, 2, 3]])
+    B1 = hyper_incidence(A, [0, 1, 2, 3])
+    B2 = hyper_incidence(A, [[0, 1, 2, 3]])
     assert np.all(B == C)
     assert np.all(B1 == A)
     assert np.all(B2 == C2)
@@ -30,6 +30,17 @@ def test_nodetoedgelist():
                   [0, 0, 1, 1],
                   [0, 0, 0, 1]])
     node_list = [[0, 1, 2, 3], [3, 4]]
-    edge_list = node_to_edge_list(A, node_list)
+    edge_list = node_to_edge(A, node_list)
     target = [[0, 1, 2], [3]]
     assert edge_list == target
+
+
+def test_convertadj():
+    A = np.array([[1, 0, 0],
+                  [1, 1, 0],
+                  [0, 1, 1],
+                  [0, 0, 1]])
+    edge_list = [[0, 1], [2]]
+    hyper_A = hyper_incidence(A, edge_list)
+    target = np.array([[1, 0], [1, 0], [1, 1], [0, 1]])
+    assert np.all(hyper_A == target)
