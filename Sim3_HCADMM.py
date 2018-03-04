f21343e0507f64d1f3f244030e6336138282f710
flyseeksky <mamengchn@gmail.com>
2018-03-04 13:01:06 -0600

    figure 2 of in-network, random graphs

---
+++ Sim3_HCADMM.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  4 12:41:42 2018
+
+@author: maxxx971
+
+This simulation verifies that HCADMM generally converges linearly and does
+speed up DCADMM. Meanwhile, communication cost goes down as well.
+
+Graphs tested:
+    1. line
+    2. star
+    3. cycle
+
+1 FC connect 30%, 60% nodes
+"""
+
